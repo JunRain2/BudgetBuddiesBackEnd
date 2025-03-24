823fea0855b53ee030f6082cd0bb34b5d3a47a79
JunRain <sa2313@tukorea.ac.kr>
2025-03-24 17:22:59 +0900

    init

---
+++ src/main/java/com/prography/budgetbuddiesbackend/BudgetBuddiesBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.prography.budgetbuddiesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetBuddiesBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetBuddiesBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prography/budgetbuddiesbackend/BudgetBuddiesBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prography.budgetbuddiesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetBuddiesBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
