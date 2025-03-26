941fe99c45c408209e7550020bf0756705bc215c
JunRain <sa2313@tukorea.ac.kr>
2025-03-26 22:28:42 +0900

    category 웹 어댑터 구현

---
+++ src/main/java/com/prography/budgetbuddiesbackend/report/adapter/in/RegisterCategoryRequest.java
@@ -0,0 +1,10 @@
+package com.prography.budgetbuddiesbackend.report.adapter.in;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class RegisterCategoryRequest {
+	private String categoryName;
+}
---
+++ src/main/java/com/prography/budgetbuddiesbackend/report/adapter/in/web/CategoryController.java
@@ -0,0 +1,47 @@
+package com.prography.budgetbuddiesbackend.report.adapter.in.web;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prography.budgetbuddiesbackend.report.adapter.in.RegisterCategoryRequest;
+import com.prography.budgetbuddiesbackend.report.adapter.in.UserCategoryListResponse;
+import com.prography.budgetbuddiesbackend.report.application.port.in.category.CategoryUseCase;
+import com.prography.budgetbuddiesbackend.report.application.port.in.category.DeleteCategoryCommand;
+import com.prography.budgetbuddiesbackend.report.application.port.in.category.RegisterCategoryCommand;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+class CategoryController {
+	private final CategoryUseCase categoryUseCase;
+
+	@GetMapping("/{userId}")
+	ResponseEntity<UserCategoryListResponse> findUserCategoryList(@PathVariable("userId") Long userId) {
+		UserCategoryListResponse response = categoryUseCase.findUserCategoryList(userId);
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/{userId}")
+	ResponseEntity<HttpStatus> registerCategory(@PathVariable Long userId, RegisterCategoryRequest request) {
+		RegisterCategoryCommand command = new RegisterCategoryCommand(userId, request.getCategoryName());
+		categoryUseCase.registerCategory(command);
+
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@DeleteMapping("/{userId}/{categoryId}")
+	ResponseEntity<HttpStatus> deleteCategory(@PathVariable Long userId, @PathVariable Long categoryId) {
+		DeleteCategoryCommand command = new DeleteCategoryCommand(userId, categoryId);
+		categoryUseCase.deleteCategory(command);
+
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+}
