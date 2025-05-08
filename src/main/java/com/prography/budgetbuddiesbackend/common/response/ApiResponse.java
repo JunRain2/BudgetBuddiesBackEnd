b48ef525d9cc5deffa065d28892aa41cd7da3056
JunRain <sa2313@tukorea.ac.kr>
2025-05-08 15:10:59 +0900

    [feat] ResponseEntity를 자동으로 감싸는 ApiResponseAdivce 구현체 구현

--- src/main/java/com/prography/budgetbuddiesbackend/common/response/ApiResponse.java
+++ src/main/java/com/prography/budgetbuddiesbackend/common/response/ApiResponse.java
@@ -2,7 +2,9 @@ package com.prography.budgetbuddiesbackend.common.response;
 
 import static com.prography.budgetbuddiesbackend.common.response.ResultCode.*;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -11,25 +13,35 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiResponse<T> {
-	private final String code;
-	private final String message;
-
+	@JsonIgnore
+	private final ResultCode code;
 	@JsonInclude(JsonInclude.Include.NON_NULL)
 	private T data;
 
 	public static ApiResponse<Void> success() {
-		return new ApiResponse<>(SUCCESS.getCode(), SUCCESS.getMessage(), null);
+		return new ApiResponse<>(SUCCESS, null);
 	}
 
 	public static <T> ApiResponse<T> success(T data) {
-		return new ApiResponse<>(SUCCESS.getCode(), SUCCESS.getMessage(), data);
+		return new ApiResponse<>(SUCCESS, data);
 	}
 
 	public static <T> ApiResponse<T> of(ResultCode code) {
-		return new ApiResponse<>(code.getCode(), code.getMessage(), null);
+		return new ApiResponse<>(code, null);
 	}
 
 	public static <T> ApiResponse<T> of(ResultCode code, T data) {
-		return new ApiResponse<>(code.getCode(), code.getMessage(), data);
+		return new ApiResponse<>(code, data);
 	}
+
+	@JsonProperty("code")
+	public String getStatus() {
+		return code.getCode();  // ResultCode 내부 status 필드
+	}
+
+	@JsonProperty("message")
+	public String getMessage() {
+		return code.getMessage(); // ResultCode 내부 message 필드
+	}
+
 }
